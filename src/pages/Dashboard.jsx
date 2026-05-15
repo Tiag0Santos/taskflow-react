2152376928cf297e63fd1d0d6108e5b70cd18528
Tiag0Santos <titaods@gmail.com>
2026-05-15 10:23:53 -0300

    feat: cards e botao nova tarefa

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,43 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
 import '../styles/Dashboard.css'
 
 function Dashboard() {
+
+  const [tasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar React',
+      status: 'Em andamento'
+    },
+
+    {
+      id: 2,
+      title: 'Criar login Firebase',
+      status: 'Pendente'
+    },
+
+    {
+      id: 3,
+      title: 'Finalizar dashboard',
+      status: 'Concluído'
+    }
+  ])
+
   return (
     <Layout>
 
-      <h1 className="dashboard-title">
-        Olá, Tiago 👋
-      </h1>
+      <div className="top-bar">
+
+        <h1 className="dashboard-title">
+          Olá, Tiago 👋
+        </h1>
+
+        <button className="new-task-btn">
+          + Nova tarefa
+        </button>
+
+      </div>
 
       <div className="cards">
 
@@ -25,6 +55,25 @@ function Dashboard() {
           <h3>Concluídas</h3>
           <p>8</p>
         </div>
+
+      </div>
+
+      <div className="tasks-container">
+
+        {tasks.map(task => (
+          <div className="task-item" key={task.id}>
+
+            <div>
+              <h4>{task.title}</h4>
+              <span>{task.status}</span>
+            </div>
+
+            <span className="task-status">
+              ✔
+            </span>
+
+          </div>
+        ))}
 
       </div>
 
